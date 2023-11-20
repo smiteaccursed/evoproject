32f1198b5f84a2c75934692f83c7be2f99392f60
_smite_ <bulat.2003.05@mail.ru>
2023-11-20 20:55:45 +0300

    add healthcheck and rename service to services

--- tests/files/run.py
+++ tests/files/run.py
@@ -1,6 +1,9 @@
 from subprocess import run
 import os
+from threading import Timer
+from logging import getLogger
 
+logger = getLogger(__name__)
 def run_all():
     e2e_policy_path = os.path.abspath("files/e2e_policy.py")
     e2e_forum_path = os.path.abspath("files/e2e_forum.py")
@@ -8,5 +11,6 @@ def run_all():
     run(["python", "-m", "unittest", e2e_policy_path])
     run(["python", "-m", "unittest", e2e_forum_path])
 if __name__ == "__main__":
-    
-    run_all()
+    logger.info("Tests will run after 10 seconds")
+    tim = Timer(15, run_all) #wait 15 seconds to start after all services
+    tim.start()
